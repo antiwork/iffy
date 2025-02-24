3234906f21bbf49e384ed967fd2f79e71f8fc76b
Razvan Marescu <razvan@marescu.net>
2025-02-24 10:19:43 -0800

    chore: bump shortest to v0.4.4

--- shortest.config.ts
+++ shortest.config.ts
@@ -4,5 +4,4 @@ export default {
   headless: false,
   baseUrl: "http://localhost:3000",
   testPattern: "e2e/**/*.test.ts",
-  anthropicKey: process.env.SHORTEST_ANTHROPIC_API_KEY,
 } satisfies ShortestConfig;
