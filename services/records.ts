antiwork/iffy

commit 4a820d2058ba75c8778adadf21515db373ec9c55

feat: wrap multi-resource database operations in transactions (#16)
* feat: wrap multi-resource database operations in transactions

- Add transaction helper to db/index.ts
- Wrap all multi-resource operations in transactions
- Keep Inngest events outside transaction scope
- Add proper error handling and rollbacks

Co-Authored-By: seth@gumroad.com <seth@gumroad.com>

* feat: add transactions to all query-then-update operations

Co-Authored-By: seth@gumroad.com <seth@gumroad.com>

* feat: add transactions to all query-then-update operations

Co-Authored-By: seth@gumroad.com <seth@gumroad.com>

* fix: return deleted rule from deleteRule transaction

Co-Authored-By: seth@gumroad.com <seth@gumroad.com>

* [autofix.ci] apply automated fixes

* feat: wrap createCustomRule in transaction for atomic operations

Co-Authored-By: seth@gumroad.com <seth@gumroad.com>

* [autofix.ci] apply automated fixes

* fix: restore important code comments

Co-Authored-By: seth@gumroad.com <seth@gumroad.com>

* nits

* spacing nit

* fix: move Inngest calls outside transactions

Co-Authored-By: seth@gumroad.com <seth@gumroad.com>

* Revert "fix: move Inngest calls outside transactions"

This reverts commit 65ea86bebe64f01a7664ed85d153bc05928dd189.

* move inngest out of the transaction

* nit

---------

Co-authored-by: Devin AI <158243242+devin-ai-integration[bot]@users.noreply.github.com>
Co-authored-by: seth@gumroad.com <seth@gumroad.com>
Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>
Co-authored-by: Seth Thompson <s3th.thompson@gmail.com>

services/records.ts
@@ -27,104 +27,112 @@ export async function createOrUpdateRecord({
   userId?: string;
   createdAt?: Date;
 }) {
-  const lastRecord = await db.query.records.findFirst({
-    where: and(eq(schema.records.clerkOrganizationId, clerkOrganizationId), eq(schema.records.clientId, clientId)),
-    columns: {
-      userId: true,
-    },
-  });
+  const record = await db.transaction(async (tx) => {
+    const lastRecord = await tx.query.records.findFirst({
+      where: and(eq(schema.records.clerkOrganizationId, clerkOrganizationId), eq(schema.records.clientId, clientId)),
+      columns: {
+        userId: true,
+      },
+    });
 
-  const [record] = await db
-    .insert(schema.records)
-    .values({
-      clerkOrganizationId,
-      clientId,
-      clientUrl,
-      name,
-      entity,
-      text,
-      imageUrls,
-      externalUrls,
-      userId,
-      createdAt,
-    })
-    .onConflictDoUpdate({
-      target: schema.records.clientId,
-      set: {
+    const [record] = await tx
+      .insert(schema.records)
+      .values({
+        clerkOrganizationId,
+        clientId,
+        clientUrl,
         name,
         entity,
         text,
         imageUrls,
         externalUrls,
-        clientUrl,
         userId,
-      },
-    })
-    .returning();
-
-  if (!record) {
-    throw new Error("Failed to upsert record");
-  }
-
-  if (record.moderationStatus === "Flagged") {
-    const userRemoved = !!lastRecord?.userId && !record.userId;
-    const userAdded = !lastRecord?.userId && !!record.userId;
-    const userChanged = !!lastRecord?.userId && !!record.userId && lastRecord.userId !== record.userId;
-
-    if (userRemoved || userChanged) {
-      await db
-        .update(schema.users)
-        .set({
-          flaggedRecordsCount: sql`${schema.users.flaggedRecordsCount} - 1`,
-        })
-        .where(and(eq(schema.users.clerkOrganizationId, clerkOrganizationId), eq(schema.users.id, lastRecord.userId!)));
-    }
-
-    if (userAdded || userChanged) {
-      await db
-        .update(schema.users)
-        .set({
-          flaggedRecordsCount: sql`${schema.users.flaggedRecordsCount} + 1`,
-        })
-        .where(and(eq(schema.users.clerkOrganizationId, clerkOrganizationId), eq(schema.users.id, record.userId!)));
-    }
-  }
-
-  return record;
-}
-
-export async function deleteRecord(clerkOrganizationId: string, recordId: string) {
-  const [record] = await db
-    .update(schema.records)
-    .set({
-      deletedAt: new Date(),
-    })
-    .where(and(eq(schema.records.clerkOrganizationId, clerkOrganizationId), eq(schema.records.id, recordId)))
-    .returning();
-
-  if (!record) {
-    throw new Error("Failed to delete record");
-  }
-
-  if (record.userId && record.moderationStatus === "Flagged") {
-    await db
-      .update(schema.users)
-      .set({
-        flaggedRecordsCount: sql`${schema.users.flaggedRecordsCount} - 1`,
+        createdAt,
       })
-      .where(and(eq(schema.users.clerkOrganizationId, clerkOrganizationId), eq(schema.users.id, record.userId)));
-  }
+      .onConflictDoUpdate({
+        target: schema.records.clientId,
+        set: {
+          name,
+          entity,
+          text,
+          imageUrls,
+          externalUrls,
+          clientUrl,
+          userId,
+        },
+      })
+      .returning();
+
+    if (!record) {
+      throw new Error("Failed to upsert record");
+    }
+
+    if (record.moderationStatus === "Flagged") {
+      const userRemoved = !!lastRecord?.userId && !record.userId;
+      const userAdded = !lastRecord?.userId && !!record.userId;
+      const userChanged = !!lastRecord?.userId && !!record.userId && lastRecord.userId !== record.userId;
+
+      if (userRemoved || userChanged) {
+        await tx
+          .update(schema.users)
+          .set({
+            flaggedRecordsCount: sql`${schema.users.flaggedRecordsCount} - 1`,
+          })
+          .where(
+            and(eq(schema.users.clerkOrganizationId, clerkOrganizationId), eq(schema.users.id, lastRecord.userId!)),
+          );
+      }
+
+      if (userAdded || userChanged) {
+        await tx
+          .update(schema.users)
+          .set({
+            flaggedRecordsCount: sql`${schema.users.flaggedRecordsCount} + 1`,
+          })
+          .where(and(eq(schema.users.clerkOrganizationId, clerkOrganizationId), eq(schema.users.id, record.userId!)));
+      }
+    }
+
+    return record;
+  });
 
   try {
     await inngest.send({
       name: "record/deleted",
-      data: { clerkOrganizationId, id: recordId },
+      data: { clerkOrganizationId, id: record.id },
     });
   } catch (error) {
     console.error(error);
   }
 
   return record;
+}
+
+export async function deleteRecord(clerkOrganizationId: string, recordId: string) {
+  return await db.transaction(async (tx) => {
+    const [record] = await tx
+      .update(schema.records)
+      .set({
+        deletedAt: new Date(),
+      })
+      .where(and(eq(schema.records.clerkOrganizationId, clerkOrganizationId), eq(schema.records.id, recordId)))
+      .returning();
+
+    if (!record) {
+      throw new Error("Failed to delete record");
+    }
+
+    if (record.userId && record.moderationStatus === "Flagged") {
+      await tx
+        .update(schema.users)
+        .set({
+          flaggedRecordsCount: sql`${schema.users.flaggedRecordsCount} - 1`,
+        })
+        .where(and(eq(schema.users.clerkOrganizationId, clerkOrganizationId), eq(schema.users.id, record.userId)));
+    }
+
+    return record;
+  });
 }
 
 export function getRecordUrls({ text, externalUrls }: { text: string; externalUrls?: string[] }) {

services/appeal-actions.ts
@@ -18,56 +18,51 @@ export async function createAppealAction({
   appealId: string;
   status: ActionStatus;
 } & ViaWithClerkUserOrUser) {
-  const lastAction = await db.query.appealActions.findFirst({
-    where: and(
-      eq(schema.appealActions.clerkOrganizationId, clerkOrganizationId),
-      eq(schema.appealActions.appealId, appealId),
-    ),
-    orderBy: desc(schema.appealActions.createdAt),
-    columns: {
-      status: true,
-    },
+  const [appealAction, lastAppealAction] = await db.transaction(async (tx) => {
+    const lastAppealAction = await tx.query.appealActions.findFirst({
+      where: and(
+        eq(schema.appealActions.clerkOrganizationId, clerkOrganizationId),
+        eq(schema.appealActions.appealId, appealId),
+      ),
+      orderBy: desc(schema.appealActions.createdAt),
+      columns: {
+        id: true,
+        status: true,
+      },
+    });
+
+    if (lastAppealAction?.status === status) {
+      return [lastAppealAction, undefined];
+    }
+
+    const [appealAction] = await tx
+      .insert(schema.appealActions)
+      .values({
+        clerkOrganizationId,
+        status,
+        appealId,
+        via,
+        clerkUserId,
+      })
+      .returning();
+
+    if (!appealAction) {
+      throw new Error("Failed to create appeal action");
+    }
+
+    // sync the record user status with the new status
+    await tx
+      .update(schema.appeals)
+      .set({
+        actionStatus: status,
+        actionStatusCreatedAt: appealAction.createdAt,
+      })
+      .where(and(eq(schema.appeals.clerkOrganizationId, clerkOrganizationId), eq(schema.appeals.id, appealId)));
+
+    return [appealAction, lastAppealAction];
   });
 
-  if (lastAction?.status === status) {
-    return lastAction;
-  }
-
-  const [appealAction] = await db
-    .insert(schema.appealActions)
-    .values({
-      clerkOrganizationId,
-      status,
-      appealId,
-      via,
-      clerkUserId,
-    })
-    .returning();
-
-  if (!appealAction) {
-    throw new Error("Failed to create appeal action");
-  }
-
-  const appeal = await db.query.appeals.findFirst({
-    where: and(eq(schema.appeals.clerkOrganizationId, clerkOrganizationId), eq(schema.appeals.id, appealId)),
-    columns: {
-      actionStatus: true,
-    },
-  });
-
-  // read the last status from the record user
-  const lastStatus = appeal?.actionStatus;
-
-  // sync the record user status with the new status
-  await db
-    .update(schema.appeals)
-    .set({
-      actionStatus: status,
-      actionStatusCreatedAt: appealAction.createdAt,
-    })
-    .where(and(eq(schema.appeals.clerkOrganizationId, clerkOrganizationId), eq(schema.appeals.id, appealId)));
-
-  if (status !== lastStatus) {
+  if (status !== lastAppealAction?.status) {
     try {
       await inngest.send({
         name: "appeal-action/status-changed",
@@ -76,7 +71,7 @@ export async function createAppealAction({
           id: appealAction.id,
           appealId,
           status,
-          lastStatus: lastStatus ?? null,
+          lastStatus: lastAppealAction?.status ?? null,
         },
       });
     } catch (error) {

services/appeals.ts
@@ -27,78 +27,80 @@ export function validateAppealToken(token: string): [isValid: false, userId: nul
 }
 
 export async function createAppeal({ userId, text }: { userId: string; text: string }) {
-  const user = await db.query.users.findFirst({
-    where: eq(schema.users.id, userId),
-    orderBy: desc(schema.userActions.createdAt),
-    with: {
-      actions: {
-        orderBy: desc(schema.appealActions.createdAt),
-        limit: 1,
+  return await db.transaction(async (tx) => {
+    const user = await tx.query.users.findFirst({
+      where: eq(schema.users.id, userId),
+      orderBy: desc(schema.userActions.createdAt),
+      with: {
+        actions: {
+          orderBy: desc(schema.appealActions.createdAt),
+          limit: 1,
+        },
       },
-    },
-  });
+    });
 
-  if (!user) {
-    throw new Error("User not found");
-  }
+    if (!user) {
+      throw new Error("User not found");
+    }
 
-  const userAction = user.actions[0];
-  if (!userAction) {
-    throw new Error("No user action found");
-  }
+    const userAction = user.actions[0];
+    if (!userAction) {
+      throw new Error("No user action found");
+    }
 
-  if (userAction.status === "Banned") {
-    throw new Error("Banned users may not appeal");
-  }
+    if (userAction.status === "Banned") {
+      throw new Error("Banned users may not appeal");
+    }
 
-  if (userAction.status !== "Suspended") {
-    throw new Error("User action is not suspended");
-  }
+    if (userAction.status !== "Suspended") {
+      throw new Error("User action is not suspended");
+    }
 
-  const { clerkOrganizationId } = user;
+    const { clerkOrganizationId } = user;
 
-  const [appeal] = await db
-    .insert(schema.appeals)
-    .values({
+    const [appeal] = await tx
+      .insert(schema.appeals)
+      .values({
+        clerkOrganizationId,
+        userActionId: userAction.id,
+      })
+      .returning();
+
+    if (!appeal) {
+      throw new Error("Failed to create appeal");
+    }
+
+    await createAppealAction({
+      clerkOrganizationId,
+      appealId: appeal.id,
+      status: "Open",
+      via: "Inbound",
+    });
+
+    await tx
+      .update(schema.messages)
+      .set({
+        appealId: appeal.id,
+      })
+      .where(
+        and(
+          eq(schema.messages.clerkOrganizationId, clerkOrganizationId),
+          eq(schema.messages.userActionId, userAction.id),
+        ),
+      );
+
+    await createMessage({
       clerkOrganizationId,
       userActionId: userAction.id,
-    })
-    .returning();
-
-  if (!appeal) {
-    throw new Error("Failed to create appeal");
-  }
-
-  await createAppealAction({
-    clerkOrganizationId,
-    appealId: appeal.id,
-    status: "Open",
-    via: "Inbound",
-  });
-
-  await db
-    .update(schema.messages)
-    .set({
+      fromId: userId,
+      text,
       appealId: appeal.id,
-    })
-    .where(
-      and(
-        eq(schema.messages.clerkOrganizationId, clerkOrganizationId),
-        eq(schema.messages.userActionId, userAction.id),
-      ),
-    );
+      type: "Inbound",
+      status: "Delivered",
+    });
 
-  await createMessage({
-    clerkOrganizationId,
-    userActionId: userAction.id,
-    fromId: userId,
-    text,
-    appealId: appeal.id,
-    type: "Inbound",
-    status: "Delivered",
+    return appeal;
   });
-
-  return appeal;
 }
 
 export async function getInboxCount(orgId: string) {

services/moderations.ts
@@ -70,90 +70,96 @@ export async function createModeration({
   testMode?: boolean;
   createdAt?: Date;
 } & ViaWithClerkUserOrUser) {
-  // read the last status from the record
-  const record = await db.query.records.findFirst({
-    where: and(eq(schema.records.clerkOrganizationId, clerkOrganizationId), eq(schema.records.id, recordId)),
-  });
+  let lastStatus: ModerationStatus | null = null;
 
-  if (!record) {
-    throw new Error("Record not found");
-  }
+  const moderation = await db.transaction(async (tx) => {
+    // read the last status from the record
+    const record = await tx.query.records.findFirst({
+      where: and(eq(schema.records.clerkOrganizationId, clerkOrganizationId), eq(schema.records.id, recordId)),
+    });
 
-  const lastStatus = record.moderationStatus;
+    if (!record) {
+      throw new Error("Record not found");
+    }
 
-  const [moderation] = await db
-    .insert(schema.moderations)
-    .values({
-      clerkOrganizationId,
-      status,
-      via,
-      clerkUserId,
-      reasoning,
-      recordId,
-      rulesetId,
-      testMode,
-      createdAt,
-    })
-    .returning();
+    lastStatus = record.moderationStatus;
 
-  if (!moderation) {
-    throw new Error("Failed to create moderation");
-  }
+    const [moderation] = await tx
+      .insert(schema.moderations)
+      .values({
+        clerkOrganizationId,
+        status,
+        via,
+        clerkUserId,
+        reasoning,
+        recordId,
+        rulesetId,
+        testMode,
+        createdAt,
+      })
+      .returning();
 
-  if (ruleIds.length > 0) {
-    await db.insert(schema.moderationsToRules).values(
-      ruleIds.map((ruleId) => ({
-        moderationId: moderation.id,
-        ruleId,
-      })),
-    );
-  }
+    if (!moderation) {
+      throw new Error("Failed to create moderation");
+    }
 
-  // sync the record status with the new status
-  await db
-    .update(schema.records)
-    .set({
-      moderationStatus: status,
-      moderationStatusCreatedAt: moderation.createdAt,
-      moderationPending: false,
-    })
-    .where(and(eq(schema.records.clerkOrganizationId, clerkOrganizationId), eq(schema.records.id, recordId)));
+    if (ruleIds.length > 0) {
+      await tx.insert(schema.moderationsToRules).values(
+        ruleIds.map((ruleId) => ({
+          moderationId: moderation.id,
+          ruleId,
+        })),
+      );
+    }
 
-  if (status !== lastStatus) {
-    if (record.userId) {
-      if (status === "Flagged") {
-        await db
-          .update(schema.users)
-          .set({
-            flaggedRecordsCount: sql`${schema.users.flaggedRecordsCount} + 1`,
-          })
-          .where(and(eq(schema.users.clerkOrganizationId, clerkOrganizationId), eq(schema.users.id, record.userId)));
-      }
-      if (lastStatus === "Flagged" && status !== "Flagged") {
-        await db
-          .update(schema.users)
-          .set({
-            flaggedRecordsCount: sql`${schema.users.flaggedRecordsCount} - 1`,
-          })
-          .where(and(eq(schema.users.clerkOrganizationId, clerkOrganizationId), eq(schema.users.id, record.userId)));
+    // sync the record status with the new status
+    await tx
+      .update(schema.records)
+      .set({
+        moderationStatus: status,
+        moderationStatusCreatedAt: moderation.createdAt,
+        moderationPending: false,
+      })
+      .where(and(eq(schema.records.clerkOrganizationId, clerkOrganizationId), eq(schema.records.id, recordId)));
+
+    if (status !== lastStatus) {
+      if (record.userId) {
+        if (status === "Flagged") {
+          await tx
+            .update(schema.users)
+            .set({
+              flaggedRecordsCount: sql`${schema.users.flaggedRecordsCount} + 1`,
+            })
+            .where(and(eq(schema.users.clerkOrganizationId, clerkOrganizationId), eq(schema.users.id, record.userId)));
+        }
+        if (lastStatus === "Flagged" && status !== "Flagged") {
+          await tx
+            .update(schema.users)
+            .set({
+              flaggedRecordsCount: sql`${schema.users.flaggedRecordsCount} - 1`,
+            })
+            .where(and(eq(schema.users.clerkOrganizationId, clerkOrganizationId), eq(schema.users.id, record.userId)));
+        }
       }
     }
 
-    if (!moderation.testMode) {
-      try {
-        await inngest.send({
-          name: "moderation/status-changed",
-          data: {
-            clerkOrganizationId,
-            id: moderation.id,
-            recordId,
-            status,
-            lastStatus,
-          },
-        });
-      } catch (error) {
-        console.error(error);
-      }
+    return moderation;
+  });
+
+  if (status !== lastStatus && !moderation.testMode) {
+    try {
+      await inngest.send({
+        name: "moderation/status-changed",
+        data: {
+          clerkOrganizationId,
+          id: moderation.id,
+          recordId,
+          status,
+          lastStatus,
+        },
+      });
+    } catch (error) {
+      console.error(error);
     }
   }
 
@@ -171,33 +177,35 @@ export async function createPendingModeration({
   recordId: string;
   createdAt?: Date;
 } & ViaWithClerkUserOrUser) {
-  const [moderation] = await db
-    .insert(schema.moderations)
-    .values({
-      clerkOrganizationId,
-      via,
-      clerkUserId,
-      recordId,
-      createdAt,
-      status: "Compliant",
-      pending: true,
-    })
-    .returning();
+  return await db.transaction(async (tx) => {
+    const [moderation] = await tx
+      .insert(schema.moderations)
+      .values({
+        clerkOrganizationId,
+        via,
+        clerkUserId,
+        recordId,
+        createdAt,
+        status: "Compliant",
+        pending: true,
+      })
+      .returning();
 
-  if (!moderation) {
-    throw new Error("Failed to create pending moderation");
-  }
+    if (!moderation) {
+      throw new Error("Failed to create pending moderation");
+    }
 
-  // update the record pending state
-  await db
-    .update(schema.records)
-    .set({
-      moderationPending: true,
-      moderationPendingCreatedAt: moderation.createdAt,
-    })
-    .where(and(eq(schema.records.clerkOrganizationId, clerkOrganizationId), eq(schema.records.id, recordId)));
+    // update the record pending state
+    await tx
+      .update(schema.records)
+      .set({
+        moderationPending: true,
+        moderationPendingCreatedAt: moderation.createdAt,
+      })
+      .where(and(eq(schema.records.clerkOrganizationId, clerkOrganizationId), eq(schema.records.id, recordId)));
 
-  return moderation;
+    return moderation;
+  });
 }
 
 export async function updatePendingModeration({
@@ -219,112 +227,121 @@ export async function updatePendingModeration({
   testMode?: boolean;
   tokens?: number;
 }) {
-  const [moderation] = await db
-    .update(schema.moderations)
-    .set({
-      status,
-      reasoning,
-      rulesetId,
-      pending: false,
-      testMode,
-      tokens,
-    })
-    .where(and(eq(schema.moderations.id, id), eq(schema.moderations.clerkOrganizationId, clerkOrganizationId)))
-    .returning();
+  let statusChanged = false;
+  let lastStatus: ModerationStatus | null = null;
 
-  if (!moderation) {
-    throw new Error("Failed to update moderation");
-  }
+  const moderation = await db.transaction(async (tx) => {
+    const [moderation] = await tx
+      .update(schema.moderations)
+      .set({
+        status,
+        reasoning,
+        rulesetId,
+        pending: false,
+        testMode,
+        tokens,
+      })
+      .where(and(eq(schema.moderations.id, id), eq(schema.moderations.clerkOrganizationId, clerkOrganizationId)))
+      .returning();
 
-  if (ruleIds.length > 0) {
-    await db.insert(schema.moderationsToRules).values(
-      ruleIds.map((ruleId) => ({
-        moderationId: moderation.id,
-        ruleId,
-      })),
-    );
-  }
+    if (!moderation) {
+      throw new Error("Failed to update moderation");
+    }
 
-  const record = await db.query.records.findFirst({
-    where: and(eq(schema.records.clerkOrganizationId, clerkOrganizationId), eq(schema.records.id, moderation.recordId)),
+    if (ruleIds.length > 0) {
+      await tx.insert(schema.moderationsToRules).values(
+        ruleIds.map((ruleId) => ({
+          moderationId: moderation.id,
+          ruleId,
+        })),
+      );
+    }
+
+    const record = await tx.query.records.findFirst({
+      where: and(
+        eq(schema.records.clerkOrganizationId, clerkOrganizationId),
+        eq(schema.records.id, moderation.recordId),
+      ),
+    });
+
+    if (!record) {
+      throw new Error("Record not found");
+    }
+
+    let updateData: Partial<typeof schema.records.$inferInsert> = {};
+
+    // read the last status from the record
+    lastStatus = record.moderationStatus;
+
+    // if the moderation is newer than the one that last updated the status,
+    // update the status
+    const statusCreatedAt = record.moderationStatusCreatedAt;
+    if (!statusCreatedAt || moderation.createdAt > statusCreatedAt) {
+      updateData.moderationStatus = status;
+      updateData.moderationStatusCreatedAt = moderation.createdAt;
+      if (status !== lastStatus) {
+        statusChanged = true;
+      }
+    }
+
+    // if the moderation is newer or the same as the one that set the pending flag,
+    // clear the pending flag
+    const pendingCreatedAt = record.moderationPendingCreatedAt;
+    if (!pendingCreatedAt || moderation.createdAt >= pendingCreatedAt) {
+      updateData.moderationPending = false;
+    }
+
+    // if no updates are needed, return the moderation
+    if (Object.keys(updateData).length === 0) {
+      return moderation;
+    }
+
+    // sync the record with the new status and/or pending flag
+    await tx
+      .update(schema.records)
+      .set(updateData)
+      .where(
+        and(eq(schema.records.clerkOrganizationId, clerkOrganizationId), eq(schema.records.id, moderation.recordId)),
+      );
+
+    if (statusChanged) {
+      if (record.userId) {
+        if (status === "Flagged") {
+          await tx
+            .update(schema.users)
+            .set({
+              flaggedRecordsCount: sql`${schema.users.flaggedRecordsCount} + 1`,
+            })
+            .where(and(eq(schema.users.clerkOrganizationId, clerkOrganizationId), eq(schema.users.id, record.userId)));
+        }
+        if (lastStatus === "Flagged" && status !== "Flagged") {
+          await tx
+            .update(schema.users)
+            .set({
+              flaggedRecordsCount: sql`${schema.users.flaggedRecordsCount} - 1`,
+            })
+            .where(and(eq(schema.users.clerkOrganizationId, clerkOrganizationId), eq(schema.users.id, record.userId)));
+        }
+      }
+    }
+
+    return moderation;
   });
 
-  if (!record) {
-    throw new Error("Record not found");
-  }
-
-  let statusChanged = false;
-  let updateData: Partial<typeof schema.records.$inferInsert> = {};
-
-  // read the last status from the record
-  const lastStatus = record.moderationStatus;
-
-  // if the moderation is newer than the one that last updated the status,
-  // update the status
-  const statusCreatedAt = record.moderationStatusCreatedAt;
-  if (!statusCreatedAt || moderation.createdAt > statusCreatedAt) {
-    updateData.moderationStatus = status;
-    updateData.moderationStatusCreatedAt = moderation.createdAt;
-    if (status !== lastStatus) {
-      statusChanged = true;
-    }
-  }
-
-  // if the moderation is newer or the same as the one that set the pending flag,
-  // clear the pending flag
-  const pendingCreatedAt = record.moderationPendingCreatedAt;
-  if (!pendingCreatedAt || moderation.createdAt >= pendingCreatedAt) {
-    updateData.moderationPending = false;
-  }
-
-  // if no updates are needed, return the moderation
-  if (Object.keys(updateData).length === 0) {
-    return moderation;
-  }
-
-  // sync the record with the new status and/or pending flag
-  await db
-    .update(schema.records)
-    .set(updateData)
-    .where(
-      and(eq(schema.records.clerkOrganizationId, clerkOrganizationId), eq(schema.records.id, moderation.recordId)),
-    );
-
-  if (statusChanged) {
-    if (record.userId) {
-      if (status === "Flagged") {
-        await db
-          .update(schema.users)
-          .set({
-            flaggedRecordsCount: sql`${schema.users.flaggedRecordsCount} + 1`,
-          })
-          .where(and(eq(schema.users.clerkOrganizationId, clerkOrganizationId), eq(schema.users.id, record.userId)));
-      }
-      if (lastStatus === "Flagged" && status !== "Flagged") {
-        await db
-          .update(schema.users)
-          .set({
-            flaggedRecordsCount: sql`${schema.users.flaggedRecordsCount} - 1`,
-          })
-          .where(and(eq(schema.users.clerkOrganizationId, clerkOrganizationId), eq(schema.users.id, record.userId)));
-      }
-    }
-
-    if (!moderation.testMode) {
-      try {
-        await inngest.send({
-          name: "moderation/status-changed",
-          data: {
-            clerkOrganizationId,
-            id,
-            recordId: moderation.recordId,
-            status,
-            lastStatus,
-          },
-        });
-      } catch (error) {
-        console.error(error);
-      }
+  if (statusChanged && !moderation.testMode) {
+    try {
+      await inngest.send({
+        name: "moderation/status-changed",
+        data: {
+          clerkOrganizationId,
+          id,
+          recordId: moderation.recordId,
+          status,
+          lastStatus,
+        },
+      });
+    } catch (error) {
+      console.error(error);
     }
   }
 

commit c3d87b93b8cb2296604baa540bedbd75cee43e10

fix(records): regression from #16

services/records.ts
@@ -106,15 +106,6 @@ export async function createOrUpdateRecord({
     return record;
   });
 
-  try {
-    await inngest.send({
-      name: "record/deleted",
-      data: { clerkOrganizationId, id: record.id },
-    });
-  } catch (error) {
-    console.error(error);
-  }
-
   return record;
 }
 
@@ -139,6 +130,15 @@ export async function deleteRecord(clerkOrganizationId: string, recordId: string
           flaggedRecordsCount: sql`${schema.users.flaggedRecordsCount} - 1`,
         })
         .where(and(eq(schema.users.clerkOrganizationId, clerkOrganizationId), eq(schema.users.id, record.userId)));
+    }
+
+    try {
+      await inngest.send({
+        name: "record/deleted",
+        data: { clerkOrganizationId, id: record.id },
+      });
+    } catch (error) {
+      console.error(error);
     }
 
     return record;