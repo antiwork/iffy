f88aa0205613b309f343a87d1d566a1caa313496
nazar <gladyshinbox@outlook.com>
2025-03-04 03:41:17 +0100

    refactor: delete old impl

--- lib/fumadocs/metadata.ts
+++
@@ -1,6 +0,0 @@
-import { createMetadataImage } from "fumadocs-core/server";
-import { source } from "@/lib/fumadocs/source";
-
-export const metadataImage: ReturnType<typeof createMetadataImage> = createMetadataImage({
-  source,
-});
--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,3 @@
-import { createMDX } from "fumadocs-mdx/next";
-
-const withMDX = createMDX();
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   async redirects() {
@@ -17,4 +13,4 @@ const nextConfig = {
   productionBrowserSourceMaps: true,
 };
 
-export default withMDX(nextConfig);
+export default nextConfig;
