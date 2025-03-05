a695ef677aa2cfc05d1ee31954c1ca7260384383
Seth Thompson <s3th.thompson@gmail.com>
2025-03-05 11:21:23 -0500

    nit

--- db/relations.ts
+++ db/relations.ts
@@ -93,7 +93,6 @@ export const appealsRelations = relations(appeals, ({ one, many }) => ({
   userAction: one(userActions, {
     fields: [appeals.userActionId],
     references: [userActions.id],
-    relationName: "fromUserAction",
   }),
   actions: many(appealActions),
 }));
