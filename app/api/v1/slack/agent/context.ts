f7f4a2246ba50c7f04bc831d12ec41985229faf9
Keyrxng <106303466+Keyrxng@users.noreply.github.com>
2025-04-21 23:04:32 +0100

    feat: enhance SlackContext with user identification and admin status checks

--- app/api/v1/slack/agent/context.ts
+++ app/api/v1/slack/agent/context.ts
@@ -2,12 +2,14 @@
 import * as schema from "@/db/schema";
 import db from "@/db";
 import { eq } from "drizzle-orm";
-import { SlackEventPayload, SupportedSlackEvents } from "./types";
 import { LogLevel, WebClient } from "@slack/web-api";
-import { decrypt } from "@/services/encrypt";
-import { SLACK_AGENT_TOOLS } from "./tools";
-import { loadModelFromConfig } from "@/services/ai";
 import { CoreMessage, generateText } from "ai";
+
+import { SlackEventPayload, SupportedSlackEvents } from "./types";
+import { decrypt } from "@/services/encrypt";
+import { getAuthorizedAgentTools } from "./tools";
+import { loadModelFromConfig } from "@/services/ai";
+import { clerkClient } from "@clerk/nextjs/server";
 
 class BaseSlackContext<T extends SupportedSlackEvents> {
   eventName: T;
@@ -59,18 +61,15 @@ class BaseSlackContext<T extends SupportedSlackEvents> {
   }
 }
 
-class SlackContext<T extends SupportedSlackEvents> extends BaseSlackContext<T> {
-  async initialize() {
-    const { payload } = this;
-    const orgDetails = await this.getOrganizationDetails(payload.teamId);
+class SlackContext<T extends SupportedSlackEvents = SupportedSlackEvents> extends BaseSlackContext<T> {
+  senderDetails: { clerkUserId: string | null; slackUserId: string } | null = null;
+  userInfoCache: Map<string, any>;
+  adminStatusCache: Map<string, boolean>;
 
-    if (orgDetails) {
-      this.payload.inbox = orgDetails.inbox;
-      this.payload.organization = orgDetails.organization;
-      this._client = new WebClient(decrypt(this.payload.inbox.inboxAccessToken), {
-        logLevel: LogLevel.ERROR,
-      });
-    }
+  constructor(event: SlackEventPayload<T>) {
+    super(event);
+    this.userInfoCache = new Map();
+    this.adminStatusCache = new Map();
   }
 
   get inbox() {
@@ -87,19 +86,109 @@ class SlackContext<T extends SupportedSlackEvents> extends BaseSlackContext<T> {
     return this.payload.organization;
   }
 
+  async initialize() {
+    const orgDetails = await this._getOrganizationDetails(this.payload.teamId);
+
+    if (orgDetails) {
+      this.payload.inbox = orgDetails.inbox;
+      this.payload.organization = orgDetails.organization;
+      this._client = new WebClient(decrypt(this.payload.inbox.inboxAccessToken), {
+        logLevel: LogLevel.ERROR,
+      });
+
+      // Pulls the clerkUserId using the slackUserId from the event
+      // This is used to identify the user in the Iffy system for agent tool authorization
+      this.senderDetails = await this.getIffyUserFromSlackId(
+        "user" in this.payload.event && this.payload.event.user
+          ? typeof this.payload.event.user === "string"
+            ? this.payload.event.user
+            : this.payload.event.user.id
+          : "user_id" in this.payload.event
+            ? this.payload.event.user_id
+            : "",
+      );
+    } else {
+      console.log("No organization found for this inbox:", this.payload.teamId);
+      throw new Error("No organization found for this inbox");
+    }
+  }
+
+  /**
+   * Retrieves the Iffy user details using the Slack user ID.
+   *
+   * Currently, we're pulling the email from the Slack user profile and using it to find the corresponding Iffy user.
+   *
+   * This method assumes that the email in the Slack profile is the same as the one used in Iffy.
+   * If the email is not found or if the user is not part of the organization, it returns null.
+   *
+   * - We could base this on the user's admin status in the Slack workspace instead but is less flexible.
+   */
+  async getIffyUserFromSlackId(slackUserId: string) {
+    const userSlackInfo = await this.client.users.info({ user: slackUserId });
+    const organization = await db.query.organizations.findFirst({
+      where: eq(schema.organizations.clerkOrganizationId, this.inbox.clerkOrganizationId),
+    });
+
+    const userEmail = userSlackInfo.user?.profile?.email;
+
+    if (!userEmail || !organization) {
+      return {
+        clerkUserId: null,
+        slackUserId: slackUserId,
+      };
+    }
+
+    // Use the email to find the corresponding Iffy user in the Clerk system
+    const {
+      data: [clerkAuthedUser],
+    } = await (
+      await clerkClient()
+    ).users.getUserList({
+      emailAddress: [userEmail],
+      limit: 1,
+    });
+
+    const { data: clerkOrgUsers } = await (
+      await clerkClient()
+    ).organizations.getOrganizationMembershipList({
+      organizationId: organization.clerkOrganizationId,
+    });
+
+    if (!clerkAuthedUser?.id) {
+      return {
+        clerkUserId: null,
+        slackUserId: slackUserId,
+      };
+    }
+
+    const isUserInOrg = clerkOrgUsers.some((user) => user.publicUserData?.userId === clerkAuthedUser.id);
+
+    return {
+      clerkUserId: isUserInOrg ? clerkAuthedUser.id : null,
+      slackUserId: slackUserId,
+    };
+  }
+
+  /**
+   * Creates a response using the LLM based on the Slack message and chat history.
+   *
+   * If tool calls are present, it will recursively call itself with the updated chat history until no tool calls are left.
+   */
   async createLlmResponse({
     slackMessage,
     chatHistory,
     username,
+    isAdmin,
   }: {
     slackMessage: string;
     username?: string;
     chatHistory?: CoreMessage[];
+    isAdmin?: boolean;
   }): Promise<string> {
     const messages: CoreMessage[] = chatHistory || [
       {
         role: "system",
-        content: this.createSlackSystemMessage(),
+        content: this._createSlackSystemMessage(isAdmin),
       },
       {
         role: "user",
@@ -107,19 +196,20 @@ class SlackContext<T extends SupportedSlackEvents> extends BaseSlackContext<T> {
       },
     ];
 
-    /**
-     * Binding ctx so methods can access it without declaring it in the function signature.
-     *
-     * The `ai` sdk doesn't support passing context directly to the tool methods or it needs
-     * defined as part of the tool `parameters` which the agent will try to produce a value for.
-     */
-    Object.values(SLACK_AGENT_TOOLS).forEach((tool) => {
-      tool.execute = tool.execute?.bind(this);
-    });
+    if (!messages.length) {
+      return "No messages to process";
+    }
+
+    if (!messages.find((msg) => msg.role === "system")) {
+      messages.unshift({
+        role: "system",
+        content: this._createSlackSystemMessage(isAdmin),
+      });
+    }
 
     const { response, toolResults, text } = await generateText({
       model: await loadModelFromConfig((config) => config.strategies.prompt.defaultModel || "gpt-4o-mini"),
-      tools: SLACK_AGENT_TOOLS,
+      tools: getAuthorizedAgentTools(this, isAdmin),
       temperature: 0.2,
       messages,
     });
@@ -159,7 +249,7 @@ class SlackContext<T extends SupportedSlackEvents> extends BaseSlackContext<T> {
   /**
    * Retrieves the organization details using the Slack team ID.
    */
-  async getOrganizationDetails(teamId: string) {
+  private async _getOrganizationDetails(teamId: string) {
     const inbox = await db.query.slackInboxes.findFirst({
       where: eq(schema.slackInboxes.slackTeamId, teamId),
     });
@@ -172,6 +262,7 @@ class SlackContext<T extends SupportedSlackEvents> extends BaseSlackContext<T> {
     const organization = await db.query.organizations.findFirst({
       where: eq(schema.organizations.clerkOrganizationId, inbox.clerkOrganizationId),
     });
+
     if (!organization) {
       console.log("No organization found for this inbox:", inbox.id);
       return null;
@@ -183,27 +274,75 @@ class SlackContext<T extends SupportedSlackEvents> extends BaseSlackContext<T> {
     };
   }
 
-  createSlackSystemMessage() {
-    return String.raw`
-    You are Iffy's Slack bot assistant. You'll be performing user actions on behalf of channel members.
-    
-    # Context:
-    ${"username" in this.payload.event ? `- User Name: ${this.payload.event.username}` : ""}
-    - Slack Team Name: ${this.inbox.slackTeamName}
-    - Slack Inbox Name: ${this.inbox.inboxName}
-    - Current Time: ${new Date().toISOString()}
-    ${"bot_id" in this.payload.event ? `- Your User ID: ${this.payload.event.bot_id}` : ""} 
-    
-    # Instructions:
-    - Ensure that all responses are appropriate and respectful.
-    - If the user asks you to perform an action, do it without asking for confirmation.
-    - If you don't understand the request, ask for clarification.
-    
-    # Constraints:
-    - NEVER tag or mention any users in the channel unless explicitly requested by the user.
-    - Use discretion when providing information about users or teams, provide only the necessary details.
-    - Do not share any sensitive information about the organization or its members.
-    `;
+  private _createSlackSystemMessage(isAdmin?: boolean) {
+    const userName = "username" in this.payload.event ? `User Name: ${this.payload.event.username}\n` : "";
+    const botId = "bot_id" in this.payload.event ? `Your User ID: ${this.payload.event.bot_id}\n` : "";
+
+    return `You are Iffy's Slack bot assistant helping users with actions in ${this.inbox.slackTeamName}.
+
+  ## Role and Context
+  - Inbox: ${this.inbox.inboxName}
+  - ${isAdmin ? "User is an admin with expanded permissions." : "User is a regular member with standard permissions."}
+  - ${userName}
+  - ${botId}
+
+  ## Communication Style
+  - Be friendly, helpful, professional and concise
+  - Use conversational but straightforward tone
+  - Respond directly to requests without unnecessary confirmations
+
+  ## Tools Usage
+  - Admin-only tools are restricted based on user permissions
+  - Administrative actions require proper Clerk OAuth authentication
+
+  ## Important Guidelines
+  - Execute requested actions promptly when authorized
+  - Ask for clarification when needed
+  - Never mention/tag users unless explicitly requested
+  - Maintain privacy and discretion with user and organization information
+  - The user will follow up with you if they need to, do not ask if they need anything else`;
+  }
+
+  /**
+   * Checks if the user is an admin in the Slack workspace.
+   *
+   * This method first checks the cache for the user's admin status. If not found,
+   * it fetches the user info from Slack and checks against the list of admin Slack user IDs for the inbox.
+   */
+  async checkPayloadUserIsAdmin() {
+    const userId: string | null =
+      "user" in this.payload.event
+        ? String(this.payload.event.user)
+        : "user_id" in this.payload.event
+          ? String(this.payload.event.user_id)
+          : null;
+
+    if (!userId) {
+      return false;
+    }
+
+    // Check cache first
+    if (this.adminStatusCache.has(userId)) {
+      const isAdmin = this.adminStatusCache.get(userId);
+      return isAdmin;
+    }
+
+    const { adminSlackUserIds } = this.inbox;
+
+    // Get user info and store it in the request-specific cache
+    let userInfo;
+    if (this.userInfoCache.has(userId)) {
+      userInfo = this.userInfoCache.get(userId);
+    } else {
+      userInfo = await this.client.users.info({ user: userId });
+      this.userInfoCache.set(userId, userInfo);
+    }
+
+    const userSlackId = userInfo.user?.id || "";
+    const isAdmin = adminSlackUserIds.some((id: string) => id === userSlackId);
+
+    this.adminStatusCache.set(userId, isAdmin);
+    return isAdmin;
   }
 }
 
